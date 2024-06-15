c202f9ef5b416a4f3482cb7b5b676c8f12c24039
Oscar Larriega <larriega@gmail.com>
2024-06-14 19:49:39 -0500

    Close mobile menu when click on a link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../provider/AuthProvider';
 
+const MIN_WIDTH_MOVIL = 767;
+
 const Header: React.FC = () => {
   const { user, logout } = useAuth();
+  const toggleNavbarButton = useRef<HTMLButtonElement>(null);
+
+  const handleClickLink = () => {
+    if (window.innerWidth <= MIN_WIDTH_MOVIL) {
+      toggleNavbarButton.current?.click();
+    }
+  };
+
   return (
     <header>
       <Navbar expand="md" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand>Music App Admin</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            ref={toggleNavbarButton}
+          />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
+              <Nav.Link as={Link} to="/" onClick={handleClickLink}>
                 Inicio
               </Nav.Link>
-              <Nav.Link as={Link} to="artists">
+              <Nav.Link as={Link} to="artists" onClick={handleClickLink}>
                 Artistas
               </Nav.Link>
-              <Nav.Link as={Link} to="album">
+              <Nav.Link as={Link} to="album" onClick={handleClickLink}>
                 Albunes
               </Nav.Link>
-              <Nav.Link as={Link} to="song">
+              <Nav.Link as={Link} to="song" onClick={handleClickLink}>
                 Canciones
               </Nav.Link>
               {user.access_token === '' ? (
